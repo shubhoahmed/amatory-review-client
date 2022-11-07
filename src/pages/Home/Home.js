c16035ea66f66ec9274b745ba441c120f1a22ca4
shubhoahmed <shubhoahmed03@gmail.com>
2022-11-08 01:08:32 +0600

    loding added

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Services from './Services';
 const Home = () => {
     return (
         <div>
-            
+            <Services></Services>
+
         </div>
     );
 };
--- src/pages/Home/Services.js
+++ src/pages/Home/Services.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import { data } from 'autoprefixer';
+import React, { useEffect, useState } from 'react';
+import Loding from '../Shared/Loading/Loding';
 
 const Services = () => {
+    const [services, setServices] = useState([]);
+    const [loading, setLoading] = useState(false);
+    useEffect(() => {
+        fetch(`http://localhost:5000/services?limit=3`)
+            .then(res => res.json())
+            .then(data => {
+                setServices(data)
+                setLoading(false)
+            })
+
+    }, []);
+
+    if (loading) {
+        return <Loding></Loding>
+    }
     return (
         <div>
-
+            {
+                services.length
+            }
         </div>
     );
 };
+
+export default Services;
