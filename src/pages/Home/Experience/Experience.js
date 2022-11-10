03ae4ee5680d24ad8e13f8378036dad7a5912468
shubhoahmed <shubhoahmed03@gmail.com>
2022-11-10 12:53:08 +0600

    ex section added

--- src/pages/Home/Experience/Experience.js
+++ src/pages/Home/Experience/Experience.js
@@ -5,7 +5,7 @@ const Experience = () => {
         <div className='my-5 mt-5'>
             <div className="hero min-h-screen bg-base-200">
                 <div className="hero-content flex-col lg:flex-row-reverse">
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/5/52/Logo-amatory.jpg" className="max-w-sm rounded-lg shadow-2xl" />
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/5/52/Logo-amatory.jpg" className="w-full rounded-lg shadow-2xl" alt='' />
                     <div>
                         <h1 className="text-5xl font-bold">Amatory New Services</h1>
                         <p className="py-6">Here you you find a new service which added last.</p>
